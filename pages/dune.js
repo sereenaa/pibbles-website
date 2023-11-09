79b7fb264d0ec543b49ac9d1ddcfd8e128ae0859
sereenaa <serenax8@gmail.com>
2023-11-10 08:28:28 +1100

    ui

--- pages/dune.js
+++ pages/dune.js
@@ -7,10 +7,12 @@ export default function Dune() {
 	return (
 		<>
 			<main className="flex min-h-screen flex-col items-center p-24">
-				<div className="z-10 max-w-5xl w-full flex flex-col items-start text-sm font-mono lg:flex">
-					<div className="mb-4">Dashboards I&apos;ve made:</div>
-				</div>
-				<div className="mb-32 grid text-center lg:text-left">
+                <div className="z-10 max-w-5xl w-full items-center font-mono text-sm lg:flex mb-2">
+                    <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                        Dashboards I&apos;ve made
+                    </p>
+                </div>
+				<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
 					<a
 						href="https://dune.com/0xpibs/gammaswap"
 						className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
--- pages/socials.js
+++ pages/socials.js
@@ -7,9 +7,11 @@ export default function Socials() {
 	return (
 		<>
 			<main className="flex min-h-screen flex-col items-center p-24">
-				<div className="z-10 max-w-5xl w-full flex flex-col items-start text-sm font-mono lg:flex">
-					<div className="mb-4">Hi!</div>
-				</div>
+				<div className="z-10 max-w-5xl w-full items-center font-mono text-sm lg:flex mb-2">
+						<p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+							Connect with me
+						</p>
+					</div>
 				<div className="mb-32 grid text-center lg:text-left">
 					<a
 						href="https://twitter.com/0xpibs"
