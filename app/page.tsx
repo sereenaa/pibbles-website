fe6d58767e2256162c070757ad9a560ef8edb6a2
sereenaa <serenax8@gmail.com>
2023-11-09 22:39:16 +1100

    escape '

--- app/page.tsx
+++ app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Projects I'm watching
+          Projects I&apos;m watching
           </p>
         </a>
 
