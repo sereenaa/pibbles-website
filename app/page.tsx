d935a8485c02423894b825fce2394eb6cd285d01
sereenaa <serenax8@gmail.com>
2023-11-10 17:47:48 +1100

    embed charts

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,17 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-7xl w-full items-center font-mono text-sm lg:flex">
+        <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] mr-3">
+          <Image
+            className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
+            src="/pibbles-profile-pic.png"
+            alt="Next.js Logo"
+            width={50}
+            height={30}
+            priority
+          />
+        </div>
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Welcome to my website!
           {/* <code className="font-mono font-bold">app/page.tsx</code> */}
@@ -29,18 +39,32 @@ export default function Home() {
         </div> */}
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/pibbles.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      
+      <br></br>
+      
+      
+      <div className="mb-32 gap-8 grid text-center lg:max-w-7xl lg:w-full lg:mb-0 lg:grid-cols-2 lg:text-left">
+        <h2 className="text-xl font-bold mb-4 text-left lg:text-left">Gammaswap</h2>
+        <h2 className="text-xl font-bold mb-4 text-left lg:text-left">Ocean Fund</h2>
+        <div className="container bg-gray-50 rounded-md">
+          <iframe 
+            src="https://dune.com/embeds/3058219/5096743/42781e08-d6a4-4aa3-a9e7-9bd7b3595c10"  // Replace with the embeddable link if available
+            width="100%" 
+            height="400" 
+          >
+          </iframe>
+        </div>
+        <div className="container bg-gray-50 rounded-md">
+          <iframe 
+            src="https://dune.com/embeds/3173881/5298175"  // Replace with the embeddable link if available
+            width="100%" 
+            height="400" 
+          >
+          </iframe>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+      <div className="mb-32 grid text-center lg:max-w-7xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
         <Link
           href="/dune"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -50,11 +74,10 @@ export default function Home() {
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Dune
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-            
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Links to all the dashboards
+            See all dashboards
           </p>
         </Link>
 
