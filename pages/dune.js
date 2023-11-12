d02030ea4b24c52b4e7d1e7beb2ae26422f2d1d4
sereenaa <serenax8@gmail.com>
2023-11-12 20:20:32 +1100

    add dashboard sections

--- pages/dune.js
+++ pages/dune.js
@@ -27,61 +27,78 @@ export default function Dune() {
 					</p>
 				</div>
 				<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-1 lg:text-left">
-					<a
-						href="https://dune.com/0xpibs/artichoke"
-						className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Image src="/artichoke.jpeg" alt="Artichoke Logo" className="mr-2 rounded-lg" width="30" height="30"/>
-						<h2 className={`text-2xl font-semibold`}>
-							Artichoke
-						</h2>
-					</a>
-					<a
-						href="https://dune.com/0xpibs/convergence"
-						className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Image src="/convergence.jpeg" alt="Convergence Logo" className="mr-2 rounded-lg" width="30" height="30"/>
-						<h2 className={`text-2xl font-semibold`}>
-							Convergence
-						</h2>
-					</a>
-					<a
-						href="https://dune.com/0xpibs/gammaswap"
-						className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Image src="/gammaswap.jpeg" alt="Gammaswap Logo" className="mr-2 rounded-md" width="30" height="30"/>
-						<h2 className={`text-2xl font-semibold`}>
-							Gammaswap
-						</h2>
-					</a>
-					<a
-						href="https://dune.com/0xpibs/modular-wallet"
-						className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Image src="/modular.jpeg" alt="Convergence Logo" className="mr-2 rounded-lg" width="30" height="30"/>
-						<h2 className={`text-2xl font-semibold`}>
-							Modular Wallet
-						</h2>
-					</a>
-					<a
-						href="https://dune.com/0xpibs/ocean-fund"
-						className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						<Image src="/ocean-fund.jpeg" alt="Convergence Logo" className="mr-2 rounded-lg" width="30" height="30"/>
-						<h2 className={`text-2xl font-semibold`}>
-							Ocean Fund
-						</h2>
-					</a>
+					<div className="p-6 mb-8 bg-black rounded-lg shadow-lg border border-gray-600 border-t-2">
+						<h2 className="text-2xl font-bold mb-2">Lending</h2>
+						<p className="mb-4">Description of lending projects</p>
+						<a
+							href="https://dune.com/0xpibs/artichoke"
+							className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image src="/artichoke.jpeg" alt="Artichoke Logo" className="mr-2 rounded-lg" width="30" height="30"/>
+							<h2 className={`text-2xl font-semibold`}>
+								Artichoke
+							</h2>
+						</a>
+					</div>
+					<div className="p-6 mb-8 bg-black rounded-lg shadow-lg border border-gray-600 border-t-2">
+						<h2 className="text-2xl font-bold mb-2">LPDfi</h2>
+						<p className="mb-4">Description of LPDfi projects</p>
+						<a
+							href="https://dune.com/0xpibs/gammaswap"
+							className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image src="/gammaswap.jpeg" alt="Gammaswap Logo" className="mr-2 rounded-md" width="30" height="30"/>
+							<h2 className={`text-2xl font-semibold`}>
+								Gammaswap
+							</h2>
+						</a>
+					</div>
+					<div className="p-6 mb-8 bg-black rounded-lg shadow-lg border border-gray-600 border-t-2">
+						<h2 className="text-2xl font-bold mb-2">Portfolio tracker</h2>
+						<p className="mb-4">Description of portfolio tracking projects</p>
+						<a
+							href="https://dune.com/0xpibs/convergence"
+							className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image src="/convergence.jpeg" alt="Convergence Logo" className="mr-2 rounded-lg" width="30" height="30"/>
+							<h2 className={`text-2xl font-semibold`}>
+								Convergence
+							</h2>
+						</a>
+						<a
+							href="https://dune.com/0xpibs/ocean-fund"
+							className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image src="/ocean-fund.jpeg" alt="Convergence Logo" className="mr-2 rounded-lg" width="30" height="30"/>
+							<h2 className={`text-2xl font-semibold`}>
+								Ocean Fund
+							</h2>
+						</a>
+					</div>
+					<div className="p-6 mb-8 bg-black rounded-lg shadow-lg border border-gray-600 border-t-2">
+						<h2 className="text-2xl font-bold mb-2">Wallet</h2>
+						<p className="mb-4">Description of wallet projects</p>
+						<a
+							href="https://dune.com/0xpibs/modular-wallet"
+							className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<Image src="/modular.jpeg" alt="Convergence Logo" className="mr-2 rounded-lg" width="30" height="30"/>
+							<h2 className={`text-2xl font-semibold`}>
+								Modular Wallet
+							</h2>
+						</a>
+					</div>
+					
 				</div>
 			</main>
 		</>
