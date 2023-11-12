62c461b152ebbd8e7fc680316d853a60b9747b3c
sereenaa <serenax8@gmail.com>
2023-11-12 19:36:12 +1100

    w-full

--- app/page.tsx
+++ app/page.tsx
@@ -127,7 +127,7 @@ export default function Home() {
   return (
     
 
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 relative">
+    <main className="flex min-h-screen w-full flex-col items-center justify-between p-24 relative">
       <div className="z-10 max-w-screen-2xl w-full items-center font-mono text-sm lg:flex">
         <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] mr-3">
           <Link href="/">
