d6cfb16b8779efdf3953e5093c82ed604c799078
sereenaa <serenax8@gmail.com>
2023-11-22 17:54:01 +1100

    add custom text for bb

--- components/footerButtons.js
+++ components/footerButtons.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function FooterButtons() {
     return (
       <>
-        <div className="mb-32 mt-16 grid text-center lg:max-w-screen-2xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <div className="mb-32 mt-16 grid text-center lg:max-w-screen-2xl lg:mb-0 lg:grid-cols-2 lg:text-left">
           <Link
             href="/dune"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -21,7 +21,7 @@ export default function FooterButtons() {
             </p>
           </Link>
 
-          <a
+          {/* <a
             href="https://debank.com/profile/0xd22c236b2de5440a0c40041f1436539a715181a1"
             className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
             target="_blank"
@@ -53,7 +53,7 @@ export default function FooterButtons() {
             <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
               Explore my NFT collection
             </p>
-          </Link>
+          </Link> */}
 
           <Link
             // href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
--- pages/socials.js
+++ pages/socials.js
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { Inter } from 'next/font/google'
 import '../app/globals.css'
 import FooterButtons from '../components/footerButtons';
+import { FiMail } from 'react-icons/fi';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,7 +11,7 @@ export default function Socials() {
 	return (
 		<>
 			<main className="flex min-h-screen flex-col items-center p-24 justify-between">
-				<div className="z-10 max-w-screen-2xl w-full items-center font-mono text-sm lg:flex mb-20">
+				<div className="z-10 max-w-screen-2xl w-full items-center font-mono text-sm lg:flex mb-10">
 					<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1] mr-3">
 						<Link href="/">
 							<Image
@@ -27,7 +28,15 @@ export default function Socials() {
 						Connect with me
 					</p>
 				</div>
-				<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-1 lg:text-left">
+				<div className="flex flex-col items-center">
+					<p className="font-mono">I also do private/custom analytics. Chuck me a DM or email and we can chat!</p>
+					<div className="animate-bounce mt-10"> {/* Animated bounce effect */}
+						<svg className="w-6 h-6 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+							<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path>
+						</svg>
+					</div>
+				</div>
+				<div className="mb-32 grid text-center lg:max-w-5xl lg:mb-0 lg:grid-cols-1 lg:text-left">
 					<a
 						href="https://twitter.com/0xpibs"
 						className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -37,6 +46,17 @@ export default function Socials() {
 						<Image src="/x.png" alt="Artichoke Logo" className="mr-2 rounded-lg" width="30" height="30"/>
 						<h2 className={`text-2xl font-semibold`}>
 							Twitter
+						</h2>
+					</a>
+					<a
+						href="mailto:pibbles.defi@gmail.com"
+						className="flex items-center group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 mt-4"  // added mt-4 for margin-top
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						<FiMail className="mr-4 ml-1 text-2xl" />
+						<h2 className={`text-2xl font-semibold`}>
+								Email
 						</h2>
 					</a>
 				</div>
